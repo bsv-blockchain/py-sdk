43eb0f4f67fe240e87740a7ed54bbee538c0b86c
Freddie Honohan <honohanf@gmail.com>
2026-01-14 17:59:52 +0900

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 with open("bsv/__init__.py") as f:
     for line in f:
         if line.startswith("__version__"):
-            version = line.strip().split("=")[1].strip(" ").strip("'")
+            version = line.strip().split("=")[1].strip().strip('"').strip("'")
             break
 
 setup(
